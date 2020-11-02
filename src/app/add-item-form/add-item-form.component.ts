5c6c60e7581be608178b6187ebfd7d45bcf74dc6
Vatibor <vatibor2@gmail.com>
2020-11-02 23:24:52 +0100

    bind the form data to main-page.component

--- src/app/add-item-form/add-item-form.component.ts
+++ src/app/add-item-form/add-item-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { BudgetItem } from '../../shared/models/budget-item.model';
 
@@ -10,6 +10,7 @@ import { BudgetItem } from '../../shared/models/budget-item.model';
 export class AddItemFormComponent implements OnInit {
 
   @Input() item: BudgetItem = new BudgetItem('', null);
+  @Output() formSubmit: EventEmitter<BudgetItem> = new EventEmitter<BudgetItem>();
 
   constructor() { }
 
@@ -17,6 +18,6 @@ export class AddItemFormComponent implements OnInit {
   }
 
   onSubmit(form: NgForm) {
-    console.log(form);
+    this.formSubmit.emit(form.value);
   }
 }
